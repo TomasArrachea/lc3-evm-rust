6985ddb6e21e15ac5a995615de2cdf8c8328bb5e
tomasarrachea <tomas.arrachea@lambdaclass.com>
2024-05-24 17:16:27 -0300

    Fix branch bug

--- src/instructions/branch.rs
+++ src/instructions/branch.rs
@@ -4,7 +4,7 @@ use crate::Register;
 pub fn branch(reg: &mut [u16], instr: u16) {
     let pc_offset = sign_extend(instr & 0x1FF, 9);
     let cond_flag = (instr >> 9) & 0x7;
-    if cond_flag & reg[Register::Cond as usize] == 1 {
-        reg[Register::Pc as usize] += pc_offset;
+    if cond_flag & reg[Register::Cond as usize] != 0 {
+        reg[Register::Pc as usize] = u16::wrapping_add(reg[Register::Pc as usize], pc_offset);
     }
 }
